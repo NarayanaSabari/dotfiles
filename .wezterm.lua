0210cfee0fdbef6c7b14788842ae334cb947c1c1
sabari <sabarinarayanakg@gmail.com>
2026-07-03 16:57:56 +0530

    wezterm: tab titles always show the directory name, not program titles

--- .wezterm.lua
+++ .wezterm.lua
@@ -17,6 +17,22 @@ config.enable_tab_bar = true
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = true
 
+-- Tab titles: always the directory name, never program-set titles
+-- (tmux windows show the session topic; WezTerm tabs show WHERE)
+wezterm.on("format-tab-title", function(tab)
+	local title = tab.active_pane.title
+	local cwd = tab.active_pane.current_working_dir
+	if cwd then
+		local path = type(cwd) == "userdata" and cwd.file_path or tostring(cwd)
+		path = path:gsub("^file://[^/]*", ""):gsub("/+$", "")
+		local base = path:match("([^/]+)$")
+		if base and #base > 0 then
+			title = base
+		end
+	end
+	return string.format(" %d: %s ", tab.tab_index + 1, title)
+end)
+
 config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.7
